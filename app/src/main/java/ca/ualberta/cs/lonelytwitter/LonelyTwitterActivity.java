a95c91c951a67a0e0fb9c165a535e72878679c37
yuenj <jyuen1@ualberta.ca>
2017-01-26 18:29:25 -0700

    lab3

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -40,6 +40,7 @@ public class LonelyTwitterActivity extends Activity {
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
 
 		try {
@@ -80,7 +81,21 @@ public class LonelyTwitterActivity extends Activity {
 				saveInFile();
 			}
 		});
+
+		clearButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+
+				tweetList.clear();
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
+			}
+		});
 	}
+
 
 	@Override
 	protected void onStart() {
